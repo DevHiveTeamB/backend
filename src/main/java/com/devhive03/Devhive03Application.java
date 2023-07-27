f299203da33df490d1616c794cc729ac771a8457
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-07-27 15:39:12 +0900

    first settiing

---
+++ src/main/java/com/devhive03/Devhive03Application.java
@@ -0,0 +1,13 @@
+package com.devhive03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devhive03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devhive03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhive03/Devhive03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhive03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devhive03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
