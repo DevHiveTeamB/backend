74fb35c03b5c658f5c9a4b6cc9ab5639cb2f9664
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-08-02 01:13:07 +0900

    Post API 개발 완료

--- src/main/java/com/devhive03/Repository/PostDAORepository.java
+++ src/main/java/com/devhive03/Repository/PostDAORepository.java
@@ -3,5 +3,8 @@ package com.devhive03.Repository;
 import com.devhive03.Model.DAO.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostDAORepository extends JpaRepository<Post, Long> {
+    List<Post> findByWriterId(Long userId);
 }
