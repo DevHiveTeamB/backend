6414ace4831714ba13f8d864c61ba2fa22419842
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-08-04 17:01:07 +0900

    Update DataLoader.java
    LectureDAORepository repository 설명 추가

--- src/main/java/com/devhive03/Bean/DataLoader.java
+++ src/main/java/com/devhive03/Bean/DataLoader.java
@@ -13,6 +13,12 @@ import java.io.IOException;
 @Configuration
 public class DataLoader {
 
+//스프링 컨테이너가 @Bean, @Component, @Repository, @Service, @Controller 등의 어노테이션이 붙은 클래스를 찾아서 빈으로 생성합니다.
+
+//스프링 컨테이너는 @Autowired, 생성자, setter 등을 통해 의존성을 주입해야 하는 필드를 찾습니다.
+
+//스프링 컨테이너는 해당 타입의 빈을 찾아서 필드에 주입합니다. 이 때, 해당 타입의 빈이 없거나 둘 이상이면 문제가 발생할 수 있습니다.
+
     @Bean
     CommandLineRunner loadData(LectureDAORepository repository) {
         return args -> {
