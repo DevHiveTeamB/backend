893a7717eb31cb355724a906a36343174e6a611f
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-08-11 21:53:28 +0900

    base URL 설정 -> "/api/"

--- src/main/java/com/devhive03/Config/SwaggerConfig.java
+++ src/main/java/com/devhive03/Config/SwaggerConfig.java
@@ -2,18 +2,21 @@ package com.devhive03.Config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+        return new OpenAPI().servers(List.of(new Server().url("/api/")))
                 .info(new Info()
                         .title("DevHive API")
                         .version("1.0")
-                        .description("This is a DevHive API."));
+                        .description("Base URL: /api/"));
     }
 }
